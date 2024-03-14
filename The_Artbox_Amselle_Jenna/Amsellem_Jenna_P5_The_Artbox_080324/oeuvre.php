d2d65c030e436acfcfd83bbae6a08abcaa78bc7e
Pearl Design <pearldesign06@gmail.com>
2024-03-14 15:09:17 +0100

    modification du code de la page oeuvre pour le simplifié

--- The_Artbox_Amselle_Jenna/Amsellem_Jenna_P5_The_Artbox_080324/oeuvre.php
+++ The_Artbox_Amselle_Jenna/Amsellem_Jenna_P5_The_Artbox_080324/oeuvre.php
@@ -1,26 +1,20 @@
 <?php require_once(__DIR__ . '\include\header.php'); ?>
 <?php
 // Récupérer l'ID de l'œuvre depuis l'URL
-if (isset($_GET['id'])) {
-    $workId = $_GET['id'];
+$workId = isset($_GET['id']) ? $_GET['id'] : null;
 
-    // Inclure le fichier avec les détails des œuvres
-    include('include/oeuvres.php');
+// Inclure le fichier avec les détails des œuvres
+include('include/oeuvres.php');
 
-    // Vérifier si l'ID existe dans le tableau des détails
-    if (array_key_exists($workId, $workDetails)) {
-        // Récupérer les détails de l'œuvre
-        $work = $workDetails[$workId];
-    } else {
-        // Rediriger si l'ID n'existe pas dans le tableau
-        header("Location: index.php");
-        exit();
-    }
-} else {
-    // Rediriger si l'ID n'est pas présent
+// Vérifier si l'ID existe dans le tableau des détails
+if (!array_key_exists($workId, $workDetails)) {
+    // Rediriger si l'ID n'existe pas dans le tableau
     header("Location: index.php");
     exit();
 }
+
+// Récupérer les détails de l'œuvre
+$work = $workDetails[$workId];
 ?>
 <main>
     <article id="detail-oeuvre">
