e38b239178f14f1d0ba5a3be3f6e9203b01a2c7a
Pearl Design <pearldesign06@gmail.com>
2024-03-21 15:05:42 +0100

    modification du fichier oeuvres en réduisant le tableau

--- The_Artbox_Amselle_Jenna/Amsellem_Jenna_P5_The_Artbox_080324/include/header.php
+++ The_Artbox_Amselle_Jenna/Amsellem_Jenna_P5_The_Artbox_080324/include/header.php
@@ -10,7 +10,7 @@
 </head>
 <body>   
 <header>
-        <a href="index.html"><img src="img/logo.png" alt="Logo Artbox" id="logo"></a>
+        <a href="index.php"><img src="img/logo.png" alt="Logo Artbox" id="logo"></a>
         <nav>
             <ul>
                 <li><a href="index.php">Accueil</a></li>
